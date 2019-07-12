82f1da5ba45c7a7a8a014c92bd21ea4cffcca6e9
Taeah Kim <taeahh.kim@gmail.com>
2019-07-11 21:46:10 -0400

    First Update

--- battleship.js
+++ battleship.js
@@ -1,6 +1,7 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = randomLoc + 1;
+var location3 = randomLoc + 2;
 var guess;
 var hits = 0;
 var guesses = 0;
